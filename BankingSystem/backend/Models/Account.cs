047ce89e5182bf6b3eaae3fe345b7b823219db41
Henrique Oliveira <mhenriqueof@outlook.com>
2025-09-12 21:31:39 -0300

    Add BankingSystem

---
+++ BankingSystem/backend/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace BankingSystem.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Holder { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+    }
+}
