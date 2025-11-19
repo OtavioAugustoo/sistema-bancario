180682401c0b89999359ae26d1c8a88254e910d1
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-11-19 16:18:31 -0300

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+//cola o react no nav
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//estilização
+import './index.css';
+
+//pro index saber oq renderizar
+import App from './App';
+
+//lugar onde todo o react vai ser injetado
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+//renderização
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-reportWebVitals();
