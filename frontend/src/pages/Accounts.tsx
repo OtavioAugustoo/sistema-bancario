02713abd2de77adb82a7b8d9cf65ff9b2bfa8ec2
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-11-19 16:17:57 -0300

    Update Accounts.tsx

--- frontend/src/pages/Accounts.tsx
+++ frontend/src/pages/Accounts.tsx
@@ -1,16 +1,9 @@
-//useEffect pra executar código quando o componente carrega
-//useState pra armazenar dados na memória do componente
 import { useEffect, useState } from "react";
-
-//conexao com o back
 import api from "../services/api";
 
-//hook pra mudar de pagina sem recarregar
 import { useNavigate } from "react-router-dom";
 
 
-
-//definicao da conta(toda conta tem id, titular e saldo)
 interface Account {
     id: number;
     holder: string;
@@ -21,10 +14,9 @@ interface Account {
 
 export default function Accounts() {
 
-    //accounts guarda a lista de contas,inicia vazia
+    //guarda a lista de contas,inicia vazia
     const [accounts, setAccounts] = useState<Account[]>([]);
 
-    //funcao pra direcionar pra outra tela
     const navigate = useNavigate();
 
     //funcao q busca as contas no back
@@ -62,25 +54,27 @@ export default function Accounts() {
                     >
 
 
-                        {/*informacoes de cada conta*/}
+                        
                         <div>
                             <p className="text-sm font-light text-gray-500 mb-1">Account ID: {account.id}</p>
                             <p className="text-xl font-bold text-gray-900 mb-3">Holder: {account.holder}</p>
 
 
 
-                            {/*exibe o saldo = balance*/}
+                            
                             <p className="text-2xl font-extrabold text-green-600">
-                                Balance: ${account.balance.toFixed(2)} {/*tofixed por conta das casa deciamis */}
+                                Balance: ${account.balance.toFixed(2)}
                             </p>
                         </div>
 
 
                         <div className="flex flex-col gap-2 mt-4">
+
+
                             {/* Botão de Ver Transacoes */}
                             <button
                                 
-                                onClick={() => navigate(`/transactions/${account.id}`)}//navega pra rota de transações
+                                onClick={() => navigate(`/transactions/${account.id}`)}
                                 className="py-2 bg-indigo-500 text-white font-medium rounded-lg hover:bg-indigo-600 transition duration-150"
                             >
                                 See Transactions & Operations
